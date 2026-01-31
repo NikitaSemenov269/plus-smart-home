9af5e9ef3bb985b18d9107b3a032c427ec6f812f
Nikita Semenov <nik.269@yandex.ru>
2026-01-31 16:21:07 +0300

    Добавление main классов для КонфигСервер и ДискавориСервер.

--- discovery-server/src/main/java/DiscoveryServer/DiscoveryServer.java
+++ discovery-server/src/main/java/DiscoveryServer/DiscoveryServer.java
@@ -1,3 +1,5 @@
+package DiscoveryServer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
